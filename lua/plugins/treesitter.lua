a7dc2d1c274e60d3e7be7958e9da4951032adda6
Hitesh Aggarwal <hitesh33231@gmail.com>
2022-09-01 00:12:06 +0530

    Minor change

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ if jit.os == "Windows" then
 end
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "cpp", "python", "lua", "vim" },
+	ensure_installed = { "c", "cpp", "python", "lua", "vim", "javascript" },
 	sync_install = false,
 	highlight = {
 		enable = true,
