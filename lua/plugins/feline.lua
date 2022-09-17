818d22e77eea376d9a5da8bce06a47a28c77322e
Hitesh Aggarwal <hitesh33231@gmail.com>
2022-09-17 22:46:24 +0530

    Modularized the status line

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -28,180 +28,192 @@ local vi_mode_colors = {
 	COMMAND = "aqua",
 }
 
-local neovimModeHighlight = function()
-	return {
-		fg = "darkblue",
-		bg = require("feline.providers.vi_mode").get_mode_color(),
-		style = "bold",
-		name = "NeovimModeHLColor",
-	}
-end
+local c = {
+	vim_mode = {
+		provider = {
+			name = "vi_mode",
+			opts = {
+				show_mode_name = true,
+				-- padding = "center", -- Uncomment for extra padding.
+			},
+		},
+		hl = function()
+			return {
+				fg = "darkblue",
+				bg = require("feline.providers.vi_mode").get_mode_color(),
+				style = "bold",
+				name = "NeovimModeHLColor",
+			}
+		end,
+		left_sep = "block",
+		right_sep = "right_filled",
+	},
+	gitBranch = {
+		provider = "git_branch",
+		hl = {
+			fg = "darkblue",
+			bg = "peanut",
+			style = "bold",
+		},
+		left_sep = "left_filled",
+		right_sep = "right_filled",
+	},
+	gitDiffAdded = {
+		provider = "git_diff_added",
+		hl = {
+			fg = "darkblue",
+			bg = "green",
+		},
+		left_sep = "left_rounded",
+		right_sep = "right_rounded",
+	},
+	gitDiffRemoved = {
+		provider = "git_diff_removed",
+		hl = {
+			fg = "darkblue",
+			bg = "red",
+		},
+		left_sep = "left_rounded",
+		right_sep = "right_rounded",
+	},
+	gitDiffChanged = {
+		provider = "git_diff_changed",
+		hl = {
+			fg = "darkblue",
+			bg = "fg",
+		},
+		left_sep = "left_rounded",
+		right_sep = "right_rounded",
+	},
+	separator = {
+		provider = "",
+	},
+	fileinfo = {
+		provider = {
+			name = "file_info",
+			opts = {
+				type = "relative-short",
+			},
+		},
+		hl = {
+			style = "bold",
+		},
+		left_sep = " ",
+		right_sep = " ",
+	},
+	diagnostic_errors = {
+		provider = "diagnostic_errors",
+	},
+	diagnostic_warnings = {
+		provider = "diagnostic_warnings",
+	},
+	diagnostic_hints = {
+		provider = "diagnostic_hints",
+	},
+	diagnostic_info = {
+		provider = "diagnostic_info",
+	},
+	lsp_client_names = {
+		provider = "lsp_client_names",
+		hl = {
+			fg = "darkblue",
+			bg = "purple",
+			style = "bold",
+		},
+		left_sep = "left_filled",
+		right_sep = "block",
+	},
+	file_type = {
+		provider = {
+			name = "file_type",
+			opts = {
+				filetype_icon = true,
+				case = "titlecase",
+			},
+		},
+		hl = {
+			fg = "darkblue",
+			bg = "dark_red",
+			style = "bold",
+		},
+		left_sep = "block",
+		right_sep = "block",
+	},
+	file_encoding = {
+		provider = "file_encoding",
+		hl = {
+			fg = "darkblue",
+			bg = "peanut",
+			style = "italic",
+		},
+		left_sep = "block",
+		right_sep = "block",
+	},
+	position = {
+		provider = "position",
+		hl = {
+			fg = "darkblue",
+			bg = "green",
+			style = "bold",
+		},
+		left_sep = "block",
+		right_sep = "block",
+	},
+	line_percentage = {
+		provider = "line_percentage",
+		hl = {
+			fg = "darkblue",
+			bg = "aqua",
+			style = "bold",
+		},
+		left_sep = "block",
+		right_sep = "block",
+	},
+	scroll_bar = {
+		provider = "scroll_bar",
+		hl = {
+			fg = "yellow",
+			style = "bold",
+		},
+	},
+}
+
+local left = {
+	c.vim_mode,
+	c.gitBranch,
+	c.gitDiffAdded,
+	c.gitDiffChanged,
+	c.gitDiffRemoved,
+	c.separator,
+}
+
+local middle = {
+	c.fileinfo,
+	c.diagnostic_errors,
+	c.diagnostic_warnings,
+	c.diagnostic_info,
+	c.diagnostic_hints,
+}
+
+local right = {
+	c.lsp_client_names,
+	c.file_type,
+	c.file_encoding,
+	c.position,
+	c.line_percentage,
+	c.scroll_bar,
+}
 
 local components = {
 	active = {
-		{
-			-- left side components
-			{
-				provider = {
-					name = "vi_mode",
-					opts = {
-						show_mode_name = true,
-						-- padding = "center", -- Uncomment for extra padding.
-					},
-				},
-				hl = neovimModeHighlight,
-				left_sep = "block",
-				right_sep = "right_filled",
-			},
-			{
-				provider = "git_branch",
-				hl = {
-					fg = "darkblue",
-					bg = "peanut",
-					style = "bold",
-				},
-				left_sep = "left_filled",
-				right_sep = "right_filled",
-			},
-			{
-				provider = "git_diff_added",
-				hl = {
-					fg = "darkblue",
-					bg = "green",
-				},
-				left_sep = "left_rounded",
-				right_sep = "right_rounded",
-			},
-			{
-				provider = "git_diff_removed",
-				hl = {
-					fg = "darkblue",
-					bg = "red",
-				},
-				left_sep = "left_rounded",
-				right_sep = "right_rounded",
-			},
-			{
-				provider = "git_diff_changed",
-				hl = {
-					fg = "darkblue",
-					bg = "fg",
-				},
-				left_sep = "left_rounded",
-				right_sep = "right_rounded",
-			},
-			{
-				provider = "",
-			},
-		},
-		{
-			-- middle components
-			{
-				provider = {
-					name = "file_info",
-					opts = {
-						type = "relative-short",
-					},
-				},
-				hl = {
-					style = "bold",
-				},
-				left_sep = " ",
-				right_sep = " ",
-			},
-			{
-				provider = "diagnostic_errors",
-			},
-			{
-				provider = "diagnostic_warnings",
-			},
-			{
-				provider = "diagnostic_hints",
-			},
-			{
-				provider = "diagnostic_info",
-			},
-		},
-		{
-			-- right side components
-			{
-				provider = "lsp_client_names",
-				hl = {
-					fg = "darkblue",
-					bg = "purple",
-					style = "bold",
-				},
-				left_sep = "left_filled",
-				right_sep = "block",
-			},
-			{
-				provider = {
-					name = "file_type",
-					opts = {
-						filetype_icon = true,
-						case = "titlecase",
-					},
-				},
-				hl = {
-					fg = "darkblue",
-					bg = "dark_red",
-					style = "bold",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			{
-				provider = "file_encoding",
-				hl = {
-					fg = "darkblue",
-					bg = "peanut",
-					style = "bold",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			{
-				provider = "position",
-				hl = neovimModeHighlight,
-				-- hl = {
-				-- 	fg = "darkblue",
-				-- 	bg = "dark_red",
-				-- 	style = "bold",
-				-- },
-				left_sep = "block",
-				right_sep = "block",
-			},
-			{
-				provider = "line_percentage",
-				hl = {
-					fg = "darkblue",
-					bg = "aqua",
-					style = "bold",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			{
-				provider = "scroll_bar",
-				hl = {
-          fg = "yellow",
-					style = "bold",
-				},
-			},
-		},
+		left,
+		middle,
+		right,
 	},
 	inactive = {
-		{
-			-- left side components
-		},
-		{
-			-- middle components
-			FileName,
-		},
-		{
-			-- right side components
-			{},
-		},
+		left,
+		middle,
+		right,
 	},
 }
 
