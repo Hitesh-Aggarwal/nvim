309d71a0b445c6dcdd9a893c7935b1fc180d3f14
Hitesh Aggarwal <hitesh33231@gmail.com>
2022-09-18 20:47:23 +0530

    Renamed a plugin

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -79,7 +79,7 @@ return packer.startup(function(use)
 	use("nvim-telescope/telescope-ui-select.nvim")
 	use("feline-nvim/feline.nvim")
 	use("lewis6991/gitsigns.nvim")
-  use("Hitesh-Aggarwal/feline_one_monokai")
+  use("Hitesh-Aggarwal/feline_one_monokai.nvim")
 
 	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
 
