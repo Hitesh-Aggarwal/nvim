db5e798deb85deade646492399e7b5adb3994e01
Hitesh Aggarwal <hitesh33231@gmail.com>
2022-10-07 09:09:09 +0530

    fix icons not colored in nvimtree

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,17 +1,19 @@
-local colorscheme = "one_monokai"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
-
+-- local colorscheme = "one_monokai"
+--
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found!")
+--   return
+-- end
+--
 local one_ok, onemonokai = pcall(require, "one_monokai")
 if not one_ok then
+  print("one_monokai colorscheme not found")
   return
 end
 
 onemonokai.setup {
+  use_cmd = true,
   themes = function()
     return {
       Normal = { bg = "#1e222a", fg = "#abb2bf" },
